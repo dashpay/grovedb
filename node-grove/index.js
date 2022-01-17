47b36da51a400e75b356fedf8d4a75614e718f4f
Ivan Shumkov <ivan@shumkov.ru>
2022-01-17 22:19:10 +0700

    chore: update tests and fix formatting

--- node-grove/index.js
+++ node-grove/index.js
@@ -1,98 +1,110 @@
-"use strict";
-
-const { promisify } = require("util");
+const { promisify } = require('util');
 
 // This file is crated when run `npm run build`. The actual source file that
 // exports those functions is ./src/lib.rs
-const { groveDbOpen, groveDbGet, groveDbInsert, groveDbProof, groveDbClose, groveDbStartTransaction, groveDbCommitTransaction, groveDbDelete, groveDbInsertIfNotExists } = require("../index.node");
+const {
+  groveDbOpen,
+  groveDbGet,
+  groveDbInsert,
+  groveDbClose,
+  groveDbStartTransaction,
+  groveDbCommitTransaction,
+  groveDbDelete,
+  groveDbInsertIfNotExists,
+} = require('../index.node');
 
 // Convert the DB methods from using callbacks to returning promises
 const groveDbGetAsync = promisify(groveDbGet);
 const groveDbInsertAsync = promisify(groveDbInsert);
 const groveDbInsertIfNotExistsAsync = promisify(groveDbInsertIfNotExists);
 const groveDbDeleteAsync = promisify(groveDbDelete);
-const groveDbProofAsync = promisify(groveDbProof);
 const groveDbCloseAsync = promisify(groveDbClose);
 const groveDbStartTransactionAsync = promisify(groveDbStartTransaction);
 const groveDbCommitTransactionAsync = promisify(groveDbCommitTransaction);
 
 // Wrapper class for the boxed `Database` for idiomatic JavaScript usage
 class GroveDB {
-    constructor(db) {
-        this.db = db;
-    }
+  /**
+   * @param {string} dbPath
+   */
+  constructor(dbPath) {
+    this.db = groveDbOpen(dbPath);
+  }
 
-    static open(path) {
-        const db = groveDbOpen(path);
-        return new GroveDB(db);
-    }
+  /**
+   * @param {Buffer[]} path
+   * @param {Buffer} key
+   * @param {boolean} [useTransaction=false]
+   * @returns {Promise<Element>}
+   */
+  async get(path, key, useTransaction = false) {
+    return groveDbGetAsync.call(this.db, path, key, useTransaction);
+  }
 
-    /**
-     *
-     * @param {Buffer[]} path
-     * @param {Buffer} key
-     * @returns {Promise<Element>}
-     */
-    async get(path, key, use_transaction) {
-        return groveDbGetAsync.call(this.db, path, key, use_transaction);
-    }
+  /**
+   * @param {Buffer[]} path
+   * @param {Buffer} key
+   * @param {Element} value
+   * @param {boolean} [useTransaction=false]
+   * @returns {Promise<*>}
+   */
+  async insert(path, key, value, useTransaction = false) {
+    return groveDbInsertAsync.call(this.db, path, key, value, useTransaction);
+  }
 
-    /**
-     *
-     * @param {Buffer[]} path
-     * @param {Buffer} key
-     * @param {Element} value
-     * @returns {Promise<*>}
-     */
-    async insert(path, key, value, use_transaction) {
-        return groveDbInsertAsync.call(this.db, path, key, value, use_transaction);
-    }
+  /**
+   * @param {Buffer[]} path
+   * @param {Buffer} key
+   * @param {Element} value
+   * @param {boolean} [useTransaction=false]
+   * @return {Promise<*>}
+   */
+  async insertIfNotExists(path, key, value, useTransaction = false) {
+    return groveDbInsertIfNotExistsAsync.call(this.db, path, key, value, useTransaction);
+  }
 
-    async insert_if_not_exists(path, key, value, use_transaction) {
-        return groveDbInsertIfNotExistsAsync.call(this.db, path, key, value, use_transaction);
-    }
+  /**
+   *
+   * @param {Buffer[]} path
+   * @param {Buffer} key
+   * @param {boolean} [useTransaction=false]
+   * @return {Promise<*>}
+   */
+  async delete(path, key, useTransaction) {
+    return groveDbDeleteAsync.call(this.db, path, key, useTransaction);
+  }
 
-    async put_aux(key, value, use_transaction) {
+  /**
+   * Closes connection to the DB
+   *
+   * @returns {Promise<void>}
+   */
+  async close() {
+    return groveDbCloseAsync.call(this.db);
+  }
 
-    }
+  /**
+   * Start a transaction with isolated scope
+   *
+   * Write operations will be allowed only for the transaction
+   * until it's committed
+   *
+   * @return {Promise<void>}
+   */
+  async startTransaction() {
+    return groveDbStartTransactionAsync.call(this.db);
+  }
 
-    async delete_aux(key, use_transaction) {
-
-    }
-
-    async get_aux(key, use_transaction) {
-
-    }
-
-    /**
-     * Not implemented in GroveDB yet
-     *
-     * @returns {Promise<*>}
-     */
-    async proof(proof_queries) {
-        return groveDbProofAsync.call(this.db, proof_queries);
-    }
-
-    /**
-     * Closes connection to the DB
-     *
-     * @returns {Promise<void>}
-     */
-    async close() {
-        return groveDbCloseAsync.call(this.db);
-    }
-
-    async start_transaction() {
-        return groveDbStartTransactionAsync.call(this.db);
-    }
-
-    async commit_transaction() {
-        return groveDbCommitTransactionAsync.call(this.db);
-    }
-
-    async delete(path, key, use_transaction) {
-        return groveDbDeleteAsync.call(this.db, path, key, use_transaction);
-    }
+  /**
+   * Commit transaction
+   *
+   * Transaction should be started before
+   *
+   * @return {Promise<void>}
+   */
+  async commitTransaction() {
+    return groveDbCommitTransactionAsync.call(this.db);
+  }
 }
 
 /**
