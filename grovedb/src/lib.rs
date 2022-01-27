62c182fdcf5e8685aacfd58a708c6832879f4feb
pasta <pasta@dashboost.org>
2022-01-27 16:07:32 +0700

    return unreturned error

--- grovedb/src/lib.rs
+++ grovedb/src/lib.rs
@@ -266,7 +266,7 @@ impl GroveDb {
 
             let (key, parent_path) = path.split_last().ok_or(Error::InvalidPath("empty path"))?;
             let (mut upper_tree, prefix) = subtrees.get(parent_path, transaction)?;
-            element.insert(&mut upper_tree, key.to_vec(), transaction);
+            element.insert(&mut upper_tree, key.to_vec(), transaction)?;
             if let Some(prefix) = prefix {
                 self.get_subtrees()
                     .insert_temp_tree(parent_path, upper_tree, transaction);
