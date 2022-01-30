2bc05604938fc2dcb6f3dc2aa261275a1abbb58a
Quantum Explorer <quantum@dash.org>
2022-01-30 12:07:18 +0700

    delete up tree while empty

--- grovedb/src/operations/delete.rs
+++ grovedb/src/operations/delete.rs
@@ -4,6 +4,24 @@ use crate::{Element, Error, GroveDb};
 
 impl GroveDb {
 
+    pub fn delete_up_tree_while_empty(
+        &mut self,
+        path: &[&[u8]],
+        key: Vec<u8>,
+        transaction: Option<&OptimisticTransactionDBTransaction>,
+    ) -> Result<u16, Error> {
+        let deleted = self.delete_internal(path, key, false, transaction)?;
+        if !deleted {
+            return Ok(0 as u16);
+        }
+        let mut delete_count: u16 = 1;
+        if let Some((key, rest_path)) = path.split_last() {
+            let deleted_parent = self.delete_up_tree_while_empty(rest_path, key.to_vec(), transaction)?;
+            delete_count += deleted_parent;
+        }
+        Ok(delete_count)
+    }
+
     pub fn delete(
         &mut self,
         path: &[&[u8]],
@@ -43,37 +61,40 @@ impl GroveDb {
             let subtrees = self.get_subtrees();
             let (mut merk, prefix) = subtrees.get(path, transaction)?;
 
-            Element::delete(&mut merk, key.clone(), transaction)?;
-
             if let Element::Tree(_) = element {
-
-                if only_delete_if_empty_tree && !merk.is_empty_tree() {
+                if merk.is_empty_tree() {
+                    Element::delete(&mut merk, key.clone(), transaction)?;
+                } else if only_delete_if_empty_tree {
                     return Ok(false);
-                }
-
-                // we need to add the merk trees into the hashmap because we will use them for querying data
-                if let Some(prefix) = prefix {
-                    subtrees
-                        .insert_temp_tree_with_prefix(prefix, merk, transaction);
                 } else {
-                    subtrees
-                        .insert_temp_tree(path, merk, transaction);
-                }
+                    Element::delete(&mut merk, key.clone(), transaction)?;
 
-                // TODO: dumb traversal should not be tolerated
-                let mut concat_path: Vec<Vec<u8>> = path.iter().map(|x| x.to_vec()).collect();
-                concat_path.push(key);
-                let subtrees_paths = self.find_subtrees(concat_path, transaction)?;
+                    // we need to add the merk trees into the hashmap because we will use them for querying data
+                    if let Some(prefix) = prefix {
+                        subtrees
+                            .insert_temp_tree_with_prefix(prefix, merk, transaction);
+                    } else {
+                        subtrees
+                            .insert_temp_tree(path, merk, transaction);
+                    }
 
-                for subtree_path in subtrees_paths {
-                    // TODO: eventually we need to do something about this nested slices
-                    let subtree_path_ref: Vec<&[u8]> =
-                        subtree_path.iter().map(|x| x.as_slice()).collect();
-                    let mut subtree = subtrees.get_subtree_without_transaction(subtree_path_ref.as_slice())?;
-                    subtree.clear(transaction).map_err(|e| {
-                        Error::CorruptedData(format!("unable to cleanup tree from storage: {}", e))
-                    })?;
+                    // TODO: dumb traversal should not be tolerated
+                    let mut concat_path: Vec<Vec<u8>> = path.iter().map(|x| x.to_vec()).collect();
+                    concat_path.push(key);
+                    let subtrees_paths = self.find_subtrees(concat_path, transaction)?;
+
+                    for subtree_path in subtrees_paths {
+                        // TODO: eventually we need to do something about this nested slices
+                        let subtree_path_ref: Vec<&[u8]> =
+                            subtree_path.iter().map(|x| x.as_slice()).collect();
+                        let mut subtree = subtrees.get_subtree_without_transaction(subtree_path_ref.as_slice())?;
+                        subtree.clear(transaction).map_err(|e| {
+                            Error::CorruptedData(format!("unable to cleanup tree from storage: {}", e))
+                        })?;
+                    }
                 }
+            } else {
+                Element::delete(&mut merk, key.clone(), transaction)?;
             }
 
             self.propagate_changes(path, transaction)?;
