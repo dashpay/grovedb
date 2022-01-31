e8f5c38a59e5d859a3e0313c1559506e216dc814
Ivan Shumkov <ivan@shumkov.ru>
2022-01-31 22:52:49 +0700

    ci: pass `-y` flag

--- node-grove/docker/build-aarch64-unknown-linux-musl.sh
+++ node-grove/docker/build-aarch64-unknown-linux-musl.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+arch
+
+uname -a
+
+## Install multilib
 apt update
-apt install gcc-multilib
+apt install -y gcc-multilib
 
-# Install Node.JS
+## Install Node.JS
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs
+apt install -y nodejs
 
+## Install build target
 rustup target install aarch64-unknown-linux-musl
 
-sudo chmod -R +w /root/.cargo
+#chmod -R 777 /root/.cargo
 
 npm run build:release
