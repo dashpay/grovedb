8abca7a5cd0712931a994027fe6e9ea7848071d1
Ivan Shumkov <ivan@shumkov.ru>
2022-01-31 20:04:24 +0700

    ci: fix musl build image name

--- node-grove/docker/build-aarch64-unknown-linux-musl.sh
+++ node-grove/docker/build-aarch64-unknown-linux-musl.sh
@@ -5,6 +5,7 @@ apt install gcc-multilib
 
 # Install Node.JS
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
 
 rustup target install aarch64-unknown-linux-musl
 
