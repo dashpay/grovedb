43282735e90cf89a5a12e4d57ca068c590ea26e8
QuantumExplorer <quantum@dash.org>
2022-02-06 15:16:40 +0700

    better node hashing (#59)

--- merk/src/tree/hash.rs
+++ merk/src/tree/hash.rs
@@ -1,5 +1,7 @@
 use std::convert::TryFrom;
 
+use integer_encoding::*;
+
 /// The length of a `Hash` (in bytes).
 pub const HASH_LENGTH: usize = 32;
 
@@ -9,23 +11,33 @@ pub const NULL_HASH: Hash = [0; HASH_LENGTH];
 /// A cryptographic hash digest.
 pub type Hash = [u8; HASH_LENGTH];
 
-/// Hashes a key/value pair.
-///
-/// **NOTE:** This will panic if the key is longer than 255 bytes, or the value
-/// is longer than 65,535 bytes.
-pub fn kv_hash(key: &[u8], value: &[u8]) -> Hash {
-    // TODO: result instead of panic
+pub fn value_hash(value: &[u8]) -> Hash {
     // TODO: make generic to allow other hashers
     let mut hasher = blake3::Hasher::new();
-    // panics if key is longer than 255!
-    let key_length = u8::try_from(key.len()).expect("key must be less than 256 bytes");
-    hasher.update(&key_length.to_be_bytes());
+
+    let val_length = value.len().encode_var_vec();
+    hasher.update(val_length.as_slice());
+    hasher.update(value);
+
+    let res = hasher.finalize();
+    let mut hash: Hash = Default::default();
+    hash.copy_from_slice(res.as_bytes());
+    hash
+}
+
+/// Hashes a key/value pair.
+///
+/// The result is Hash(key_len, key, Hash(value_len, value))
+pub fn kv_hash(key: &[u8], value: &[u8]) -> Hash {
+    // TODO: make generic to allow other hashers
+    let mut hasher = blake3::Hasher::new();
+
+    let key_length = key.len().encode_var_vec();
+    hasher.update(key_length.as_slice());
     hasher.update(key);
 
-    // panics if value is longer than 65535!
-    let val_length = u16::try_from(value.len()).expect("value must be less than 65,536 bytes");
-    hasher.update(&val_length.to_be_bytes());
-    hasher.update(value);
+    let value_hash = value_hash(value);
+    hasher.update(value_hash.as_slice());
 
     let res = hasher.finalize();
     let mut hash: Hash = Default::default();
