20309dfcc22b324651160566fcf22c887133cc77
Evgeny Fomin <fominok@hotmail.com>
2022-02-08 01:19:50 +0300

    visualize grovedb

--- grovedb/src/visualize.rs
+++ grovedb/src/visualize.rs
@@ -1,9 +1,15 @@
 use std::io::{Result, Write};
 
-use crate::subtree::Element;
+use itertools::Itertools;
+use merk::Merk;
+use serde::de::IntoDeserializer;
+use storage::Storage;
+
+use crate::{subtree::Element, GroveDb};
 
 static HEX_LEN: usize = 8;
 static STR_LEN: usize = 32;
+static INDENT_SPACES: usize = 4;
 
 /// Pretty visualization of GroveDB components.
 pub(crate) trait Visualize {
@@ -29,8 +35,20 @@ impl<'a, W: Write> Drawer<'a, W> {
         self.level -= 1;
     }
 
-    pub(crate) fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        self.write.write(buf)
+    pub(crate) fn write(&mut self, buf: &[u8]) -> Result<()> {
+        let lines_iter = buf.split(|c| *c == b'\n');
+        let sep = if self.level > 0 {
+            let mut result = " ".repeat(INDENT_SPACES * self.level - 1);
+            result.insert(0, '\n');
+            result
+        } else {
+            String::new()
+        };
+        let interspersed_lines_iter = lines_iter.intersperse(sep.as_bytes());
+        for line in interspersed_lines_iter {
+            self.write.write(line)?;
+        }
+        Ok(())
     }
 }
 
@@ -40,7 +58,7 @@ fn to_hex(bytes: &[u8]) -> String {
     result
 }
 
-impl<B: AsRef<[u8]>> Visualize for B {
+impl Visualize for [u8] {
     fn visualize<'a, W: Write>(&self, mut drawer: Drawer<'a, W>) -> Result<Drawer<'a, W>> {
         let value = self.as_ref();
         let hex_repr = to_hex(value);
@@ -83,6 +101,68 @@ impl Visualize for Element {
                 drawer = hash.visualize(drawer)?;
             }
         }
+        Ok(drawer)
+    }
+}
+
+impl GroveDb {
+    fn draw_subtree<'a, W: Write>(
+        &self,
+        mut drawer: Drawer<'a, W>,
+        path: Vec<Vec<u8>>,
+    ) -> Result<Drawer<'a, W>> {
+        drawer.down();
+        let (merk, _) = self
+            .get_subtrees()
+            .get(path.iter().map(|x| x.as_slice()), None)
+            .expect("cannot find Merk");
+        let mut iter = Element::iterator(merk.raw_iter(None));
+        while let Some((key, element)) = iter.next().expect("cannot get next element") {
+            drawer.write(b"\n[key: ")?;
+            drawer = key.visualize(drawer)?;
+            drawer.write(b" ")?;
+            match element {
+                Element::Tree(_) => {
+                    drawer.write(b"tree:")?;
+                    drawer.down();
+                    let mut inner_path = path.clone();
+                    inner_path.push(key);
+                    drawer = self.draw_subtree(drawer, inner_path)?;
+                    drawer.up();
+                }
+                other => {
+                    drawer = other.visualize(drawer)?;
+                }
+            }
+        }
+        drawer.up();
+        Ok(drawer)
+    }
+
+    fn draw_root_tree<'a, W: Write>(&self, mut drawer: Drawer<'a, W>) -> Result<Drawer<'a, W>> {
+        drawer.down();
+        let keys = self.root_leaf_keys.iter().fold(
+            vec![Vec::new(); self.root_leaf_keys.len()],
+            |mut acc, (key, idx)| {
+                acc[*idx] = key.clone();
+                acc
+            },
+        );
+        for k in keys {
+            drawer.write(b"\n")?;
+            drawer.write(&k)?;
+            drawer = self.draw_subtree(drawer, vec![k])?
+        }
+        drawer.up();
+        Ok(drawer)
+    }
+}
+
+impl Visualize for GroveDb {
+    fn visualize<'a, W: Write>(&self, mut drawer: Drawer<'a, W>) -> Result<Drawer<'a, W>> {
+        drawer.write(b"root")?;
+        drawer = self.draw_root_tree(drawer)?;
+        drawer.write(b"\n")?;
         Ok(drawer)
     }
 }
