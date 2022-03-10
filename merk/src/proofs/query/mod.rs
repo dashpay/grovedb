2fc2ab3485baef764dc0d1b200900a690a090860
Wisdom Ogwu <iammadab@gmail.com>
2022-03-10 21:09:06 +0100

    adjusted range_to_proof to test for exact range match i.e not generalizing to the inclusive case

--- merk/src/proofs/query/mod.rs
+++ merk/src/proofs/query/mod.rs
@@ -1860,6 +1860,7 @@ mod test {
         let (proof, absence) = walker
             .create_full_proof(queryitems.as_slice())
             .expect("create_proof errored");
+        dbg!(&proof);
 
         let mut iter = proof.iter();
         assert_eq!(iter.next(), Some(&Op::Push(Node::KV(vec![2], vec![2]))));
@@ -1869,15 +1870,13 @@ mod test {
         assert_eq!(iter.next(), Some(&Op::Child));
         assert_eq!(iter.next(), Some(&Op::Push(Node::KV(vec![5], vec![5]))));
         assert_eq!(iter.next(), Some(&Op::Parent));
-        assert_eq!(iter.next(), Some(&Op::Push(Node::KV(vec![7], vec![7]))));
         assert_eq!(
             iter.next(),
-            Some(&Op::Push(Node::KVHash([
-                236, 141, 96, 8, 244, 103, 232, 110, 117, 105, 162, 111, 148, 9, 59, 195, 2, 250,
-                165, 180, 215, 137, 202, 221, 38, 98, 93, 247, 54, 180, 242, 116
+            Some(&Op::Push(Node::Hash([
+                133, 188, 175, 131, 60, 89, 221, 135, 133, 53, 205, 110, 58, 56, 128, 58, 1, 227,
+                75, 122, 83, 20, 125, 44, 149, 44, 62, 130, 252, 134, 105, 200
             ])))
         );
-        assert_eq!(iter.next(), Some(&Op::Parent));
         assert_eq!(iter.next(), Some(&Op::Child));
         assert!(iter.next().is_none());
         assert_eq!(absence, (true, false));
