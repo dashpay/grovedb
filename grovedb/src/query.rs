799efe1780a5d5f7c2b840b22d21b729bfc5e176
Wisdom Ogwu <iammadab@gmail.com>
2022-06-22 22:02:09 +0100

    handles merging of arbitrary sized same length paths

--- grovedb/src/query.rs
+++ grovedb/src/query.rs
@@ -46,13 +46,15 @@ impl PathQuery {
             PathQuery::new_unsized(p1.path.clone(), combined_query)
         } else {
             let paths: Vec<&[Vec<u8>]> = vec![&p1.path, &p2.path];
-            let queries = vec![p1.query.query.clone(), p2.query.query.clone()];
+            let path_queries = vec![p1, p2];
+            // let queries = vec![p1.query.query.clone(), p2.query.query.clone()];
 
             let (common_path, next_index) = PathQuery::get_common_path(vec![&p1.path, &p2.path]);
             dbg!(&common_path);
-            dbg!(next_index);
+            // dbg!(next_index);
+            // dbg!(p1.path.len());
 
-            let query = PathQuery::build_query(paths, queries, next_index);
+            let query = PathQuery::build_query(path_queries, next_index, p1.path.len() - 1);
             dbg!(&query);
             // at this point, we need to create a new function that takes the paths and the
             // index then builds up the query.
@@ -60,9 +62,21 @@ impl PathQuery {
         }
     }
 
-    fn build_query(paths: Vec<&[Vec<u8>]>, queries: Vec<Query>, start_index: usize) -> Query {
+    // when building conditional, we want to recurse on build query
+    // but when we get to the end of the path, we want to get the query for that
+    // path we have an array of paths and queries, we need to preserve the index
+    // values we care about actually, we need to know the position of a path
+    // maybe it makes more sense to pass the path query that way we always have
+    // access to the query
+
+    fn build_query(path_queries: Vec<&PathQuery>, start_index: usize, last_index: usize) -> Query {
+        dbg!(start_index);
+        dbg!(last_index);
         let mut level = start_index;
-        let keys_at_level = paths.iter().map(|&path| &path[level]).collect::<Vec<_>>();
+        let keys_at_level = path_queries
+            .iter()
+            .map(|&path_query| &path_query.path[level])
+            .collect::<Vec<_>>();
 
         // we need to group the paths based on their distinct nature
         let mut path_branches: HashMap<_, Vec<usize>> = HashMap::new();
@@ -80,15 +94,37 @@ impl PathQuery {
 
         dbg!(&path_branches);
 
+        // the key in path_branches represents all the paths that have the same keys up
+        // to that point based on the assumption that all paths are equal, then
+        // if two paths have the same values at that point when
+        // we should combine their queries and use the resulting query for the
+        // conditional.
+
+        // for each grouped key, we want to create a path
         let mut query = Query::new();
         for (key, value) in path_branches.drain() {
+            dbg!("loop started");
             query.insert_key(key.to_vec());
-            query.add_conditional_subquery(
-                QueryItem::Key(key.to_vec()),
-                None,
-                Some(queries[value[0]].clone()),
-            );
+
+            dbg!("start_index at loop", start_index);
+            dbg!("last_index at loop", last_index);
+            let next_query = if start_index == last_index {
+                // use the query from the path query
+                dbg!("they are equal should not call build again");
+                path_queries[value[0]].query.query.clone()
+            } else {
+                dbg!("I got called");
+                let mut new_path_queries = vec![];
+                for a in value {
+                    new_path_queries.push(path_queries[a]);
+                }
+                Self::build_query(new_path_queries, start_index + 1, last_index)
+            };
+            dbg!("end of call");
+
+            query.add_conditional_subquery(QueryItem::Key(key.to_vec()), None, Some(next_query));
         }
+        dbg!("loop ended");
 
         query
     }
@@ -232,19 +268,19 @@ mod tests {
         let mut query_two = Query::new();
         query_two.insert_all();
 
-        let mut subq = Query::new();
-        subq.insert_all();
-
-        query_two.set_subquery(subq);
         let path_query_two = PathQuery::new_unsized(
-            vec![b"deep_leaf".to_vec(), b"deep_node_2".to_vec()],
+            vec![
+                b"deep_leaf".to_vec(),
+                b"deep_node_2".to_vec(),
+                b"deeper_node_4".to_vec(),
+            ],
             query_two,
         );
 
         let proof = temp_db.prove(&path_query_two).unwrap().unwrap();
         let (_, result_set_two) = GroveDb::execute_proof(proof.as_slice(), &path_query_two)
             .expect("should execute proof");
-        assert_eq!(result_set_two.len(), 5);
+        assert_eq!(result_set_two.len(), 2);
 
         let merged_path_query = PathQuery::merge(&path_query_one, &path_query_two);
         assert_eq!(merged_path_query.path, vec![b"deep_leaf".to_vec()]);
@@ -253,15 +289,12 @@ mod tests {
         let proof = temp_db.prove(&merged_path_query).unwrap().unwrap();
         let (_, result_set_merged) = GroveDb::execute_proof(proof.as_slice(), &merged_path_query)
             .expect("should execute proof");
-        assert_eq!(result_set_merged.len(), 8);
+        assert_eq!(result_set_merged.len(), 5);
 
         let keys = [
             b"key4".to_vec(),
             b"key5".to_vec(),
             b"key6".to_vec(),
-            b"key7".to_vec(),
-            b"key8".to_vec(),
-            b"key9".to_vec(),
             b"key10".to_vec(),
             b"key11".to_vec(),
         ];
@@ -269,9 +302,6 @@ mod tests {
             b"value4".to_vec(),
             b"value5".to_vec(),
             b"value6".to_vec(),
-            b"value7".to_vec(),
-            b"value8".to_vec(),
-            b"value9".to_vec(),
             b"value10".to_vec(),
             b"value11".to_vec(),
         ];
