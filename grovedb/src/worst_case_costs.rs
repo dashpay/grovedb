358bfac19fb69228f1f94456d814206330ddfbc2
Wisdom Ogwu <iammadab@gmail.com>
2022-08-18 09:03:46 +0100

    cleaned up insertion function

--- grovedb/src/worst_case_costs.rs
+++ grovedb/src/worst_case_costs.rs
@@ -70,18 +70,30 @@ impl GroveDb {
         max_element_number: u32,
         max_key_size: u32,
     ) {
-        // For worst case conditions:
-        // - merk tree was just opened hence only root node and corresponding links are
-        //   loaded
+        // Insertion Process
+        // - Walk from the root to insertion point (marking every node in path as
+        //   modified)
+        // - Insert new node (also mark as modified)
+        // - Perform rotation
+        //      - Worst case scenario, we rotate into an already occupied slot
+        //      - In that case, we need to fetch that node from backing store to move it
+        //        (also marked as modified)
+        // - Lastly we commit
+        //      - Write all modified nodes to the backing store.
 
-        // maximum height of a tree
-        // 1.44 * log n
-        // subtracting 1 from the max_element_number as we can't be adding an element to
-        // an already filled tree.
+        // For worst case conditions, the merk tree must have just been opened. This way each
+        // walk will need us to get to the backing store.
+
+        // Root is already loaded on merk open, so initial walk to insertion point only loads
+        // nodes after root from backing store.
+        // Here we calculate the number of walks we would have to perform.
+
+        // We reduce by 1 because we are given the max element number, you can't add after max
+        // so worst case has to be 1 element away from max
         let worst_case_element_number = max_element_number - 1;
+        // With this we can calculate the maximum tree height root inclusive
         let max_tree_height = (1.44 * (worst_case_element_number as f32).log2()).floor() as u32;
-
-        // to insert a node, we have to walk from the root to some leaf node
+        // max_number_of_walks is basically max_tree_height - 1 (because we exclude the root)
         let max_number_of_walks = max_tree_height - 1;
 
         // for each walk, we have to seek and load from storage (equivalent to a get)
@@ -93,55 +105,46 @@ impl GroveDb {
         // this requires computing the value_hash and kv_hash
         cost.hash_node_calls += 2;
 
-        dbg!(&cost);
-        dbg!(&cost.storage_loaded_bytes - 3 * Self::worst_case_encoded_link_size(max_key_size));
+        // dbg!(&cost);
+        // dbg!(&cost.storage_loaded_bytes - 4 * Self::worst_case_encoded_link_size(max_key_size));
         // dbg!(Self::worst_case_encoded_link_size(max_key_size));
         // dbg!(Self::worst_case_encoded_kv_node_size(max_element_size));
 
-        // on insertion, we need to balance the tree
-        // worst case for insertion, one rotation at some subtree point A
-        // where a rotation can be single or double.
-        // at most one rotation, because before insertion the tree is balanced
-        // after insertion the height of a subtree on the insertion path is increased by
-        // one after rotation, the height of that subtree is same as it was
-        // before insertion hence the nodes above do not need to change.
-        // Summary: insertion leads to an unbalance at a single point, once we fix that
-        // point the tree is balanced.
+        // After insertion, we need to balance the tree
+        // ordinarily balancing a tree has no extra cost, as it just moves around already
+        // modified nodes, but there are times when we try to rotate into an already occupied slot.
+        // In such cases, we need to fetch what is currently in that slot (from the backing store)
+        // remove it, rotate and then reattach to rotated node.
+        // Merk marks every attached node as modified.
 
-        // Effects of rotation
-        // Rotation rearranges nodes on the insertion path
-        // Since merk already marks all nodes on the insertion path as modified hash re
-        // computation will be performed. We are concerned more with the effect
-        // of rotating towards an already occupied point In this case we first
-        // have to detach the node at the target location, and connect it to the node we
-        // are rotating. Merk marks any moved node as modified even when their
-        // children do not change (this feels inefficient and unnecessary) TODO:
-        // Look into if there is a legitimate reason for doing this. Hence worst
-        // case, we have an additional modified node during the insertion.
+        // TODO: This might be 2 look into this.
+        // Add 1 get cost for the node in rotation spot
+        let mut modified_node_count = max_number_of_walks + 1;
+        GroveDb::add_worst_case_get_merk_node(cost, max_key_size, max_element_size);
 
-        // Adding 2 new nodes, 1 for the root node and another for the newly inserted
-        // node remember walks started after the root node (this node might have
-        // changed so we need to re-commit) TODO: ignoring rotations for now,
-        // handle later
-        let max_number_of_modified_nodes = max_number_of_walks + 2;
-        dbg!(max_number_of_modified_nodes);
+        // During the commit phase
+        // We update the backing store state
+        // We write all the modified nodes + the root node + newly inserted node
+        // i.e max_number_of_walks + 2 extra nodes
+        modified_node_count += 2;
 
-        // commit stage
-        // for every modified node, recursively call commit on all modified children
-        // at base, write the node to storage
-        // we create a batch entry [prefixed_key, encoded_tree]
-        // the prefix is created during get storage context for merk open
-        let prefix_size: u32 = 32;
-        let prefixed_key_size = prefix_size + max_key_size;
+        // let max_number_of_modified_nodes = max_number_of_walks + 2 + 1;
+        // dbg!(max_number_of_modified_nodes);
+
+        // When writing a key value pair to the backing store
+        // the key has to prefixed with a 32 byte hash
+        // and the value is the encoded tree node
+        let prefixed_key_size = 32 + max_key_size;
         let value_size = Self::worst_case_encoded_tree_size(max_key_size, max_element_size);
 
-        for _ in 0..max_number_of_modified_nodes {
+        for _ in 0..modified_node_count {
             cost.seek_count += 1;
             cost.hash_node_calls += 1;
             cost.storage_written_bytes += (prefixed_key_size + value_size)
         }
 
-        // sub one hash node call because we don't hash the root
+        // Reduce the hash node calls count by 1 because the root node is not rehashed on commit
+        // when you call merk.root_hash() the hash is computed in real time.
         cost.hash_node_calls -= 1;
 
         // Write the root key
@@ -331,7 +334,7 @@ mod test {
         // all the trees we walk get modified so they have to be re-written
 
         let mut worst_case_cost = OperationCost::default();
-        GroveDb::add_worst_case_insert_merk_node(&mut worst_case_cost, 60, 11, 8);
+        GroveDb::add_worst_case_insert_merk_node(&mut worst_case_cost, 1, 6, 1);
         dbg!(&worst_case_cost);
 
         // Open a merk and insert 10 elements.
@@ -341,20 +344,25 @@ mod test {
         let mut merk = Merk::open(storage.get_storage_context(empty()).unwrap())
             .unwrap()
             .expect("cannot open merk");
-        let batch = make_batch_seq(1..11);
-        for b in batch {
-            merk.apply::<_, Vec<_>>(&[b.clone()], &[]).unwrap().unwrap()
-        }
-        // let m = merk.apply::<_, Vec<_>>(batch.as_slice(), &[]);
-        // let a = vec![b"1".to_vec(), b"2".to_vec(), b"3".to_vec(), b"4".to_vec(),
-        // b"5".to_vec(), b"6".to_vec(), b"7".to_vec(), b"8".to_vec(), b"9".to_vec(),
-        // b"10".to_vec()]; for m in a {
-        //     println!();
-        //     println!("inserting {}", std::str::from_utf8(&m).unwrap());
-        //     merk.apply::<_, Vec<_>>(&[(m, Op::Put(b"a".to_vec()))], &[])
-        //         .unwrap()
-        //         .unwrap();
+        // let batch = make_batch_seq(1..11);
+        // for b in batch {
+        //     merk.apply::<_, Vec<_>>(&[b.clone()], &[]).unwrap().unwrap()
         // }
+        // let m = merk.apply::<_, Vec<_>>(batch.as_slice(), &[]);
+        let a = vec![
+            b"5".to_vec(),
+            b"4".to_vec(),
+            b"7".to_vec(),
+            b"6".to_vec(),
+            b"8".to_vec(),
+        ];
+        for m in a {
+            // println!();
+            // println!("inserting {}", std::str::from_utf8(&m).unwrap());
+            merk.apply::<_, Vec<_>>(&[(m, Op::Put(b"a".to_vec()))], &[])
+                .unwrap()
+                .unwrap();
+        }
 
         // // drop merk, so nothing is stored in memory
         dbg!("dropping merk");
@@ -365,8 +373,8 @@ mod test {
             .unwrap()
             .expect("cannot open merk");
 
-        let batch = make_batch_seq(11..12);
-        let actual_cost = merk.apply::<_, Vec<_>>(batch.as_slice(), &[]);
+        // let batch = make_batch_seq(11..12);
+        let actual_cost = merk.apply::<_, Vec<_>>(&[(b"9".to_vec(), Op::Put(b"a".to_vec()))], &[]);
         dbg!(&actual_cost);
 
         assert_eq!(actual_cost.cost, worst_case_cost);
