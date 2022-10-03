78c0acf369c86493bc176a1965c4f2fabbf743f0
Wisdom Ogwu <iammadab@gmail.com>
2022-10-03 07:36:53 +0100

    add test for sum item

--- grovedb/src/tests.rs
+++ grovedb/src/tests.rs
@@ -3994,3 +3994,55 @@ fn test_sum_tree_behaves_like_regular_tree() {
         Element::new_item(vec![3])
     );
 }
+
+#[test]
+fn test_sum_item_behaves_like_regular_item() {
+    let db = make_grovedb();
+    db.insert([TEST_LEAF], b"sumkey", Element::empty_sum_tree(), None)
+        .unwrap()
+        .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"sumkey"],
+        b"k1",
+        Element::new_item(vec![1]),
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"sumkey"],
+        b"k2",
+        Element::new_sum_item(vec![1]),
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert([TEST_LEAF, b"sumkey"], b"k3", Element::empty_tree(), None)
+        .unwrap()
+        .expect("should insert tree");
+
+    // Test proper item retrieval
+    let item = db
+        .get([TEST_LEAF, b"sumkey"], b"k2", None)
+        .unwrap()
+        .expect("should get item");
+    assert_eq!(item, Element::new_sum_item(vec![1]));
+
+    // Test proof generation
+    let mut query = Query::new();
+    query.insert_key(b"k2".to_vec());
+
+    let path_query = PathQuery::new_unsized(vec![TEST_LEAF.to_vec(), b"sumkey".to_vec()], query);
+    let proof = db
+        .prove_query(&path_query)
+        .unwrap()
+        .expect("should generate proof");
+    let (root_hash, result_set) =
+        GroveDb::verify_query(&proof, &path_query).expect("should verify proof");
+    assert_eq!(root_hash, db.db.root_hash(None).unwrap().unwrap());
+    assert_eq!(result_set.len(), 1);
+    assert_eq!(
+        Element::deserialize(&result_set[0].1).expect("should deserialize element"),
+        Element::new_sum_item(vec![1])
+    );
+}
