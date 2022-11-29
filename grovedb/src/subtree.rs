1b69b89e969da0dbb3ed684fe9716784f8bad596
Wisdom Ogwu <iammadab@gmail.com>
2022-11-29 07:30:07 +0100

    add constructor methods for sum tree and sum item + add sum_value method

--- grovedb/src/subtree.rs
+++ grovedb/src/subtree.rs
@@ -105,12 +105,28 @@ impl Element {
         Element::new_tree_with_flags(Default::default(), flags)
     }
 
+    pub fn empty_sum_tree() -> Self {
+        Element::new_sum_tree(Default::default())
+    }
+
+    pub fn empty_sum_tree_with_flags(flags: Option<ElementFlags>) -> Self {
+        Element::new_sum_tree_with_flags(Default::default(), flags)
+    }
+
     pub fn new_item(item_value: Vec<u8>) -> Self {
         Element::Item(item_value, None)
     }
 
     pub fn new_item_with_flags(item_value: Vec<u8>, flags: Option<ElementFlags>) -> Self {
         Element::Item(item_value, flags)
+    }
+
+    pub fn new_sum_item(value: i64) -> Self {
+        Element::SumItem(value.encode_var_vec(), None)
+    }
+
+    pub fn new_sum_item_with_flags(value: i64, flags: Option<ElementFlags>) -> Self {
+        Element::SumItem(value.encode_var_vec(), flags)
     }
 
     pub fn new_reference(reference_path: ReferencePathType) -> Self {
@@ -148,6 +164,34 @@ impl Element {
         flags: Option<ElementFlags>,
     ) -> Self {
         Element::Tree(maybe_root_key, flags)
+    }
+
+    pub fn new_sum_tree(maybe_root_key: Option<Vec<u8>>) -> Self {
+        Element::SumTree(maybe_root_key, 0, None)
+    }
+
+    pub fn new_sum_tree_with_flags(maybe_root_key: Option<Vec<u8>>, flags: Option<ElementFlags>) -> Self {
+        Element::SumTree(maybe_root_key, 0, flags)
+    }
+
+    pub fn new_sum_tree_with_flags_and_sum_value(
+        maybe_root_key: Option<Vec<u8>>,
+        sum_value: SumValue,
+        flags: Option<ElementFlags>,
+    ) -> Self {
+        Element::SumTree(maybe_root_key, sum_value, flags)
+    }
+
+    /// Decoded the integer value in the SumItem element type, returns 0 for
+    /// everything else
+    pub fn sum_value(&self) -> Option<i64> {
+        match self {
+            Element::SumItem(value, _) => {
+                i64::decode_var(value).map(|(encoded_value, _)| encoded_value)
+            }
+            Element::SumTree(_, sum_value, _) => Some(*sum_value),
+            _ => Some(0),
+        }
     }
 
     /// Grab the optional flag stored in an element
