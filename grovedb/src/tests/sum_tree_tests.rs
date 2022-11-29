717286cd48d8e973feb65adbd0b7d0478abc65e7
Wisdom Ogwu <iammadab@gmail.com>
2022-11-29 08:52:21 +0100

    add failing test for sum tree propagation

--- grovedb/src/tests/sum_tree_tests.rs
+++ grovedb/src/tests/sum_tree_tests.rs
@@ -4,6 +4,7 @@ use merk::{
 };
 
 use crate::{
+    reference_path::ReferencePathType,
     tests::{make_test_grovedb, TEST_LEAF},
     Element, Error, GroveDb, PathQuery,
 };
@@ -406,4 +407,133 @@ fn test_sum_tree_feature() {
     assert_eq!(merk.sum(), Some(9999940)); // 30 + 10 - 100 + 10000000
 
     // TODO: Test out overflows
+}
+
+#[test]
+fn test_sum_tree_propagation() {
+    let db = make_test_grovedb();
+    // Tree
+    //   SumTree
+    //      SumTree
+    //        Item1
+    //        SumItem1
+    //        SumItem2
+    db.insert([TEST_LEAF], b"key", Element::empty_sum_tree(), None, None)
+        .unwrap()
+        .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"key"],
+        b"tree2",
+        Element::empty_sum_tree(),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"key", b"tree2"],
+        b"item1",
+        Element::new_item(vec![2]),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert item");
+    db.insert(
+        [TEST_LEAF, b"key", b"tree2"],
+        b"sumitem1",
+        Element::new_sum_item(5),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert item");
+    db.insert(
+        [TEST_LEAF, b"key", b"tree2"],
+        b"sumitem2",
+        Element::new_sum_item(10),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert item");
+    db.insert(
+        [TEST_LEAF, b"key", b"tree2"],
+        b"item2",
+        Element::new_reference(ReferencePathType::AbsolutePathReference(vec![
+            TEST_LEAF.to_vec(),
+            b"key".to_vec(),
+            b"tree2".to_vec(),
+            b"sumitem1".to_vec(),
+        ])),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert item");
+
+    let sum_tree = db
+        .get([TEST_LEAF], b"key", None)
+        .unwrap()
+        .expect("should fetch tree");
+    assert_eq!(sum_tree.sum_value(), Some(15));
+
+    // Assert node feature types
+    let test_leaf_merk = db
+        .open_non_transactional_merk_at_path([TEST_LEAF])
+        .unwrap()
+        .expect("should open tree");
+    assert!(matches!(
+        test_leaf_merk
+            .get_feature_type(b"key")
+            .unwrap()
+            .expect("node should exist"),
+        Some(BasicMerk)
+    ));
+
+    let parent_sum_tree = db
+        .open_non_transactional_merk_at_path([TEST_LEAF, b"key"])
+        .unwrap()
+        .expect("should open tree");
+    assert!(matches!(
+        parent_sum_tree
+            .get_feature_type(b"tree2")
+            .unwrap()
+            .expect("node should exist"),
+        Some(SummedMerk(15)) /* 15 because the child sum tree has one sum item of value 5 and
+                              * another of value 10 */
+    ));
+
+    let child_sum_tree = db
+        .open_non_transactional_merk_at_path([TEST_LEAF, b"key", b"tree2"])
+        .unwrap()
+        .expect("should open tree");
+    assert!(matches!(
+        child_sum_tree
+            .get_feature_type(b"item1")
+            .unwrap()
+            .expect("node should exist"),
+        Some(SummedMerk(0))
+    ));
+    assert!(matches!(
+        child_sum_tree
+            .get_feature_type(b"sumitem1")
+            .unwrap()
+            .expect("node should exist"),
+        Some(SummedMerk(5))
+    ));
+    assert!(matches!(
+        child_sum_tree
+            .get_feature_type(b"sumitem2")
+            .unwrap()
+            .expect("node should exist"),
+        Some(SummedMerk(10))
+    ));
+    assert!(matches!(
+        child_sum_tree
+            .get_feature_type(b"item2")
+            .unwrap()
+            .expect("node should exist"),
+        Some(SummedMerk(0))
+    ));
 }
