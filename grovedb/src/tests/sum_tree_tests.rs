dc98aa03573e27080713e1b962b963884356b2e3
Wisdom Ogwu <iammadab@gmail.com>
2022-11-29 08:11:21 +0100

    test sum item behaviour

--- grovedb/src/tests/sum_tree_tests.rs
+++ grovedb/src/tests/sum_tree_tests.rs
@@ -72,3 +72,69 @@ fn test_sum_tree_behaves_like_regular_tree() {
         Element::new_item(vec![3])
     );
 }
+
+#[test]
+fn test_sum_item_behaves_like_regular_item() {
+    let db = make_test_grovedb();
+    db.insert(
+        [TEST_LEAF],
+        b"sumkey",
+        Element::empty_sum_tree(),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"sumkey"],
+        b"k1",
+        Element::new_item(vec![1]),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"sumkey"],
+        b"k2",
+        Element::new_sum_item(5),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+    db.insert(
+        [TEST_LEAF, b"sumkey"],
+        b"k3",
+        Element::empty_tree(),
+        None,
+        None,
+    )
+    .unwrap()
+    .expect("should insert tree");
+
+    // Test proper item retrieval
+    let item = db
+        .get([TEST_LEAF, b"sumkey"], b"k2", None)
+        .unwrap()
+        .expect("should get item");
+    assert_eq!(item, Element::new_sum_item(5));
+
+    // Test proof generation
+    let mut query = Query::new();
+    query.insert_key(b"k2".to_vec());
+
+    let path_query = PathQuery::new_unsized(vec![TEST_LEAF.to_vec(), b"sumkey".to_vec()], query);
+    let proof = db
+        .prove_query(&path_query)
+        .unwrap()
+        .expect("should generate proof");
+    let (root_hash, result_set) =
+        GroveDb::verify_query(&proof, &path_query).expect("should verify proof");
+    assert_eq!(root_hash, db.grove_db.root_hash(None).unwrap().unwrap());
+    assert_eq!(result_set.len(), 1);
+    let element_from_proof =
+        Element::deserialize(&result_set[0].1).expect("should deserialize element");
+    assert_eq!(element_from_proof, Element::new_sum_item(5));
+    assert_eq!(element_from_proof.sum_value(), Some(5));
+}
