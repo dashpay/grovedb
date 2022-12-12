0f9ae7acd88f148120a4c499e5665666e20ba0b1
Quantum Explorer <quantum@dash.org>
2022-12-13 02:42:56 +0700

    feat: added some convenience methods for estimated costs

--- grovedb/src/operations/get.rs
+++ grovedb/src/operations/get.rs
@@ -474,6 +474,8 @@ where {
         cost
     }
 
+    /// Get the Operation Cost for a has query that doesn't follow
+    /// references with the following parameters
     pub fn average_case_for_has_raw(
         path: &KeyInfoPath,
         key: &KeyInfo,
@@ -491,6 +493,29 @@ where {
         cost
     }
 
+    /// Get the Operation Cost for a has query where we estimate that we
+    /// would get a tree with the following parameters
+    pub fn average_case_for_has_raw_tree(
+        path: &KeyInfoPath,
+        key: &KeyInfo,
+        estimated_flags_size: u32,
+        is_sum_tree: bool,
+        in_parent_tree_using_sums: bool,
+    ) -> OperationCost {
+        let mut cost = OperationCost::default();
+        GroveDb::add_average_case_has_raw_tree_cost::<RocksDbStorage>(
+            &mut cost,
+            path,
+            key,
+            estimated_flags_size,
+            is_sum_tree,
+            in_parent_tree_using_sums,
+        );
+        cost
+    }
+
+    /// Get the Operation Cost for a get query that doesn't follow
+    /// references with the following parameters
     pub fn average_case_for_get_raw(
         path: &KeyInfoPath,
         key: &KeyInfo,
@@ -508,6 +533,7 @@ where {
         cost
     }
 
+    /// Get the Operation Cost for a get query with the following parameters
     pub fn average_case_for_get(
         path: &KeyInfoPath,
         key: &KeyInfo,
@@ -523,6 +549,26 @@ where {
             in_parent_tree_using_sums,
             estimated_element_size,
             estimated_references_sizes,
+        );
+        cost
+    }
+
+    /// Get the Operation Cost for a get query with the following parameters
+    pub fn average_case_for_get_tree(
+        path: &KeyInfoPath,
+        key: &KeyInfo,
+        estimated_flags_size: u32,
+        is_sum_tree: bool,
+        in_parent_tree_using_sums: bool,
+    ) -> OperationCost {
+        let mut cost = OperationCost::default();
+        GroveDb::add_average_case_get_raw_tree_cost::<RocksDbStorage>(
+            &mut cost,
+            path,
+            key,
+            estimated_flags_size,
+            is_sum_tree,
+            in_parent_tree_using_sums,
         );
         cost
     }
