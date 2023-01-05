66ff1bcbbdd2b234bc5e47c88471d65435f52c3a
Quantum Explorer <quantum@dash.org>
2023-01-06 00:48:35 +0700

    fmt

--- merk/src/merk/mod.rs
+++ merk/src/merk/mod.rs
@@ -335,7 +335,6 @@ where
     /// Note that this is essentially the same as a normal RocksDB `get`, so
     /// should be a fast operation and has almost no tree overhead.
     pub fn exists(&self, key: &[u8]) -> CostResult<bool, Error> {
-
         self.has_node_direct(key)
     }
 
@@ -343,8 +342,8 @@ where
     ///
     /// Note that this is essentially the same as a normal RocksDB `get`, so
     /// should be a fast operation and has almost no tree overhead.
-    /// Contrary to a simple exists, this traverses the tree and can be faster if the tree is
-    /// cached, but slower if it is not
+    /// Contrary to a simple exists, this traverses the tree and can be faster
+    /// if the tree is cached, but slower if it is not
     pub fn exists_by_traversing_tree(&self, key: &[u8]) -> CostResult<bool, Error> {
         self.has_node(key)
     }
@@ -371,7 +370,11 @@ where
     }
 
     /// Returns the feature type for the node at the given key.
-    pub fn get_feature_type(&self, key: &[u8], allow_cache: bool) -> CostResult<Option<TreeFeatureType>, Error> {
+    pub fn get_feature_type(
+        &self,
+        key: &[u8],
+        allow_cache: bool,
+    ) -> CostResult<Option<TreeFeatureType>, Error> {
         if allow_cache {
             self.get_node_fn(key, |node| {
                 node.feature_type().wrap_with_cost(Default::default())
@@ -395,7 +398,11 @@ where
 
     /// Gets the value hash of a node by a given key, `None` is returned in case
     /// when node not found by the key.
-    pub fn get_value_hash(&self, key: &[u8], allow_cache: bool) -> CostResult<Option<CryptoHash>, Error> {
+    pub fn get_value_hash(
+        &self,
+        key: &[u8],
+        allow_cache: bool,
+    ) -> CostResult<Option<CryptoHash>, Error> {
         if allow_cache {
             self.get_node_fn(key, |node| {
                 (*node.value_hash()).wrap_with_cost(OperationCost::default())
@@ -409,7 +416,11 @@ where
 
     /// Gets a hash of a node by a given key, `None` is returned in case
     /// when node not found by the key.
-    pub fn get_kv_hash(&self, key: &[u8], allow_cache: bool) -> CostResult<Option<CryptoHash>, Error> {
+    pub fn get_kv_hash(
+        &self,
+        key: &[u8],
+        allow_cache: bool,
+    ) -> CostResult<Option<CryptoHash>, Error> {
         if allow_cache {
             self.get_node_fn(key, |node| {
                 (*node.inner.kv.hash()).wrap_with_cost(OperationCost::default())
@@ -479,14 +490,13 @@ where
 
     /// Generic way to get a node's field
     fn get_node_direct_fn<T, F>(&self, key: &[u8], f: F) -> CostResult<Option<T>, Error>
-        where
-            F: FnOnce(&Tree) -> CostContext<T>,
+    where
+        F: FnOnce(&Tree) -> CostContext<T>,
     {
         Tree::get(&self.storage, key).flat_map_ok(|maybe_node| {
-                            let mut cost = OperationCost::default();
-                            Ok(maybe_node.map(|node| f(&node).unwrap_add_cost(&mut cost)))
-                                .wrap_with_cost(cost)
-                        })
+            let mut cost = OperationCost::default();
+            Ok(maybe_node.map(|node| f(&node).unwrap_add_cost(&mut cost))).wrap_with_cost(cost)
+        })
     }
 
     /// Generic way to get a node's field
