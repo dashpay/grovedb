076c027b4da68e48fbbd415ceb7f82a63041818a
Paul DeLucia <69597248+pauldelucia@users.noreply.github.com>
2023-01-31 15:55:30 +0700

    Update lib.rs

--- grovedb/src/lib.rs
+++ grovedb/src/lib.rs
@@ -136,7 +136,6 @@ pub type TransactionArg<'db, 'a> = Option<&'a Transaction<'db>>;
 #[cfg(feature = "full")]
 impl GroveDb {
     /// Opens an instance of GroveDB at the given path.
-    /// This automatically creates an empty root tree.
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         let db = RocksDbStorage::default_rocksdb_with_path(path)?;
         Ok(GroveDb { db })
