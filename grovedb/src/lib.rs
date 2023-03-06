c0ff522cb9e01be85521a67708c1438fc1bed88b
Wisdom Ogwu <iammadab@gmail.com>
2023-03-06 09:53:59 +0100

    wip

--- grovedb/src/lib.rs
+++ grovedb/src/lib.rs
@@ -69,7 +69,7 @@ use costs::{
     cost_return_on_error, cost_return_on_error_no_add, CostResult, CostsExt, OperationCost,
 };
 #[cfg(feature = "full")]
-use element::helpers::raw_decode;
+use crate::helpers::raw_decode;
 #[cfg(any(feature = "full", feature = "verify"))]
 use element::helpers;
 #[cfg(any(feature = "full", feature = "verify"))]
