cf5e472cd0aa8ff382d199180705c517816e50ef
Quantum Explorer <quantum@dash.org>
2023-09-28 10:32:29 +0700

    fix

--- merk/src/tree/ops.rs
+++ merk/src/tree/ops.rs
@@ -598,7 +598,7 @@ where
                                 cost_return_on_error!(
                                     &mut cost,
                                     tree.apply_sorted(
-                                        batch,
+                                        &batch[..index],
                                         old_specialized_cost,
                                         value_defined_cost_fn,
                                         update_tree_value_based_on_costs,
@@ -654,7 +654,7 @@ where
                                 cost_return_on_error!(
                                     &mut cost,
                                     tree.apply_sorted(
-                                        batch,
+                                        &batch[index + 1..],
                                         old_specialized_cost,
                                         value_defined_cost_fn,
                                         update_tree_value_based_on_costs,
