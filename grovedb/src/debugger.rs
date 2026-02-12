3c68b0c295ccb3517eacab38cd138736e5ce3ca3
Quantum Explorer <quantum@dash.org>
2026-02-12 07:53:15 +0000

    more work

--- grovedb/src/debugger.rs
+++ grovedb/src/debugger.rs
@@ -719,6 +719,12 @@ fn element_to_grovedbg(element: crate::Element) -> grovedbg_types::Element {
                 element_flags,
             }
         }
+        crate::Element::CommitmentTree(root_key, element_flags) => {
+            grovedbg_types::Element::Subtree {
+                root_key,
+                element_flags,
+            }
+        }
     }
 }
 
